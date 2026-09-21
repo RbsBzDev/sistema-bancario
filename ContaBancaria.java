e36a2f98d0c84d633f3d603e6935111283bd4156
RbsBzDev <chicletao3@gmail.com>
2026-09-21 19:36:21 -0300

    Adição e Correção de funções

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -2,9 +2,9 @@ public class ContaBancaria {
 
     private String titular;
     private String numeroConta;
-    private double saldo;}
+    private double saldo;
 
-    public contaBancaria(String titular, String numeroConta, double saldoInicial) {
+    public ContaBancaria(String titular, String numeroConta, double saldoInicial) {
         this.titular = titular;
         this.numeroConta = numeroConta;
         this.saldo = saldoInicial;
@@ -13,25 +13,29 @@ public class ContaBancaria {
     public void exibirDados() {
         System.out.println("Titular: " + titular);
         System.out.println("Conta: " + numeroConta);
-        System.out.printf("Saldo: R$ $.2f%n", saldo);
+        System.out.printf("Saldo: R$ %.2f%n", saldo);
     }
 
     public void depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
             System.out.println("Depósito realizado com sucesso.");
+            System.out.printf("Saldo atual: R$ %.2f%n", saldo);
         } else {
             System.out.println("O valor do depósito tem que ser maior que zero.");
         }
     }
 
     public void sacar(double valor) {
-        if (valor <=0) {
+        if (valor <= 0) {
             System.out.println("O valor do saque deve ser maior que zero.");
         } else if (valor > saldo) {
-            System.out.println("Saldo insuficiente");
+            System.out.println("Saldo insuficiente.");
+            System.out.printf("Saldo atual: R$ %.2f%n", saldo);
         } else {
             saldo -= valor;
-            System.out.println("Saque realizado com sucesso");
+            System.out.println("Saque realizado com sucesso.");
+            System.out.printf("Saldo atual: R$ %.2f%n", saldo);
         }
     }
+}
